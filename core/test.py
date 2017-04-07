50a9881a22ea53b0a77c53332a3a0d35fc3f090e
zerolocusta <zerolocust@gmail.com>
2017-04-07 18:02:24 +0800

    update server

--- core/test.py
+++ core/test.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
-import tornado.web
+import tornado.web as web
+import tornado.gen as gen
 import os
 
 static_path = os.path.join(os.path.dirname(__file__), "output")
@@ -7,17 +8,21 @@ settings = {
     "static_path" : static_path
 }
 
+
 class MainHandler(tornado.web.RequestHandler):
+
+    @gen.coroutine
     def get(self):
-        self.write("Hello, world")
+        self.redirect("/demo.htm")
 
 def make_app():
     return tornado.web.Application([
-        (r'/(.*)', tornado.web.StaticFileHandler, {'path': static_path}),
+        (r'/', MainHandler),
+        (r'/(.*)', web.StaticFileHandler, {'path': static_path}),
     ], **settings)
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8090)
+    app.listen(8888)
     print(static_path)
     tornado.ioloop.IOLoop.current().start()
